cd71c0ecb2463948d083168312dfb33b8417bccd
Deep Jana <124864072+Ex0rtic@users.noreply.github.com>
2025-02-11 11:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ SportsCar.prototype.getTopSpeed =()=>{
 	return this.topSpeed;
 }
 SportsCar.prototype=Object.create(Car.prototype);
-SportsCar.prototype.constructor=Car;
+SportsCar.prototype.constructor=SportsCar
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
