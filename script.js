54c0b16fe5b99908d8816246a1713fca3c7c663f
Deep Jana <124864072+Ex0rtic@users.noreply.github.com>
2025-02-11 10:35:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed = topSpeed;
+}
 
+Car.prototype.getMakeModel = () =>{
+	return `${this.make} ${this.model}`;
+}
+
+SportsCar.prototype.getTopSpeed =()=>{
+	return this.topSpeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
